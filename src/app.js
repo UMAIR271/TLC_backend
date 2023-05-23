9a863aa94168b5daad741970213529240cb20539
Manish <manish@dowellresearch.in>
2023-05-24 03:24:51 +0530

    user models,auth controller,user route ,middleware

--- src/app.js
+++ src/app.js
@@ -9,4 +9,17 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use(cookieParser())
 
+app.use("/api/v1/", routes)
+
+app.get("/", (_req, res) => {
+    res.send("All services are running fine. Stop doubting the backend, you frontend programmer!")
+})
+
+app.all("*", (_req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: "Route not found"
+    })
+})
+
 export default app;
