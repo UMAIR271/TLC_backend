f1853aae8452ac3814972ec2ac15b3f1686225db
Manish <manish@dowellresearch.in>
2023-05-27 03:36:40 +0530

    Product Schema, controller

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
 import authRoutes from "./auth.route.js"
 import collectionRoutes from "./collection.route.js"
-import swaggerUI from 'swagger-ui-express'
-import swaggerDocument from '../../api.json' assert { type: "json" }
+import productRoute from "./product.route.js"
 
 const router = Router()
 
 router.use("/auth", authRoutes)
 router.use("/collection", collectionRoutes)
-router.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
+router.use("/product", productRoute)
 
 export default router
