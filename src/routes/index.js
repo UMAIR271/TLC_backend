2dbc1182ac6af446d798f29b8274eb3c7d81d07a
Manish <manish@dowellresearch.in>
2023-05-29 07:54:44 +0530

    Coupon models , controller , routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import authRoutes from "./auth.route.js"
 import collectionRoutes from "./collection.route.js"
 import productRoute from "./product.route.js"
+import couponRoute from "./coupon.route.js";
 
 const router = Router()
 
 router.use("/auth", authRoutes)
 router.use("/collection", collectionRoutes)
 router.use("/product", productRoute)
+router.use("/coupon", couponRoute)
 
 export default router
